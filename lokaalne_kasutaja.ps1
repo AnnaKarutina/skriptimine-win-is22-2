f6fc20fc698b0c4b61ba323e45679e5b21668e10
Anna Karutina <anna.karutina@voco.ee>
2022-11-08 15:37:16 +0200

    Lokaalne kasutaja etteantud kasutaja nime ja parooliga

---
+++ lokaalne_kasutaja.ps1
@@ -0,0 +1,4 @@
+﻿# loome kasutaja jaoks parool
+$KasutajaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+# loome kasutaja
+New-LocalUser 'kasutaja1' -Password $KasutajaParool -FullName 'Esimene kasutaja' -Description 'Local account'
