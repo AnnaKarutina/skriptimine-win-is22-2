1199c39989137e4ef7a9401bda5dd39b9e2bc577
Anna Karutina <anna.karutina@voco.ee>
2022-11-09 11:40:36 +0200

    Kasutusjuhend vale käivitamise puhul

--- lokaalne_kasutaja_parameetritega.ps1
+++ lokaalne_kasutaja_parameetritega.ps1
@@ -1,10 +1,13 @@
-﻿# vajalikud parameetrid
-param(
-$KasutajaNimi,
-$TaisNimi,
-$KontoKirjeldus
-)
-# loome kasutaja jaoks parool
-$KasutajaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
-# loome kasutaja
-New-LocalUser $KasutajaNimi -Password $KasutajaParool -FullName $TaisNimi -Description $KontoKirjeldus
+﻿# kontroll parameetrite arvule
+if($args.Count -ne 3){
+    echo "Kasutusjuhend: .\skriptifailinimi.ps1 kasutajanimi täisnimi kirjeldus"
+} else {
+    # vajalikud parameetrid
+    $KasutajaNimi = $args[0]
+    $TaisNimi = $args[1]
+    $KontoKirjeldus = $args[2]
+    # loome kasutaja jaoks parool
+    $KasutajaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+    # loome kasutaja
+    New-LocalUser $KasutajaNimi -Password $KasutajaParool -FullName $TaisNimi -Description $KontoKirjeldus
+}
