ef8a4b50a21f4314172bdd3fa44c791e6be2cd76
Anna Karutina <anna.karutina@voco.ee>
2022-11-09 11:20:27 +0200

    Lokaalne kasutaja käsurea parameetritega

---
+++ lokaalne_kasutaja_parameetritega.ps1
@@ -0,0 +1,10 @@
+﻿# vajalikud parameetrid
+param(
+$KasutajaNimi,
+$TaisNimi,
+$KontoKirjeldus
+)
+# loome kasutaja jaoks parool
+$KasutajaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+# loome kasutaja
+New-LocalUser $KasutajaNimi -Password $KasutajaParool -FullName $TaisNimi -Description $KontoKirjeldus
